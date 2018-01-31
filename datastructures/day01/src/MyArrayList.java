78efd4cd1af1b8290297c4ecdb381d8a1a7c92ae
askrishnan <anupamakrishnan@students.olin.edu>
2018-01-30 22:49:13 -0500

    Adding Day01

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -1,42 +1,65 @@
+import java.util.ArrayList;
+
 public class MyArrayList {
-    private Cow[] elems;
+    private Cow[] cows;
     private int size;
 
     // TODO: Runtime: O(?)
     public MyArrayList() {
-        // TODO
+        cows = new Cow[10];
+        size = 0;
     }
 
     // TODO: Runtime: O(?)
     public MyArrayList(int capacity) {
-        // TODO
+        cows = new Cow[capacity];
+        size = 0;
     }
 
     // TODO: Runtime: O(?)
     public void add(Cow c) {
-        // TODO
+        this.add(c);
+        size++;
+
     }
 
     // TODO: Runtime: O(?)
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
     // TODO: Runtime: O(?)
     public Cow get(int index) {
-        // TODO
-        return null;
+        indexCheck(index);
+        return cows[index];
     }
 
     // TODO: Runtime: O(?)
     public Cow remove(int index) {
-        // TODO
-        return null;
+        indexCheck(index);
+        Cow reMooved = cows[index];
+        for (int i = index; i <= size(); i++){
+            cows[i] = cows[i+1];
+        }
+        size--;
+        return reMooved;
     }
 
     // TODO: Runtime: O(?)
     public void add(int index, Cow c) {
-        // TODO
+        indexCheck(index);
+        if (size() == cows.length) {
+            resize();
+        }
+        for (int i = size(); i > index; i--) {
+            cows[i] = cows[i - 1];
+        }
+        cows[index] = c;
+        size++;
     }
+
+    private void indexCheck(int index) {
+        if (index >= size() || index < 0)
+            throw new IndexOutOfBoundsException();
+        }
 }
