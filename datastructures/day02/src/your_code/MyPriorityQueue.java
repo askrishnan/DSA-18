ca56e0f5d0ff46c8c2dd654c7782a6d3826168a9
Anusha Datar <anushadatar@gmail.com>
2018-02-01 22:11:10 -0500

    Day 2 homework

--- datastructures/day02/src/your_code/MyPriorityQueue.java
+++ datastructures/day02/src/your_code/MyPriorityQueue.java
@@ -1,20 +1,42 @@
 package your_code;
 
-/**
- * An implementation of a priority Queue
- */
-public class MyPriorityQueue {
+import com.sun.jdi.connect.Connector;
 
-    public void enqueue(int item) {
-        // TODO
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+public class MyPriorityQueue {
+    // Priority queue implementation.
+
+
+    private ArrayList<Integer> al;
+
+
+    public MyPriorityQueue() {
+        // Creates new PriorityQueue with ArrayList implementation.
+        al = new ArrayList<>();
     }
 
-    /**
-     * Return and remove the largest item on the queue.
-     */
+    public void enqueue(int item) {
+        // Adds item to the priority queue.
+        // item : integer to add.
+        al.add(item);
+    }
     public int dequeueMax() {
-        // TODO
-        return -1;
+        // Removes maximum value from the queue.
+        // returns int value of the maximum.
+        int max = al.get(0);
+        int index = 0;
+        for (int i = 0; i < al.size(); i++) {
+            if (al.get(i) > max) {
+                max = al.get(i);
+                index = i;
+            }
+        }
+        al.remove(index);
+        return max;
     }
 
 }
--- datastructures/day02/src/your_code/MyStack.java
+++ datastructures/day02/src/your_code/MyStack.java
@@ -2,6 +2,7 @@ package your_code;
 import ADTs.StackADT;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * An implementation of the Stack interface.
@@ -9,20 +10,33 @@ import java.util.LinkedList;
 public class MyStack implements StackADT<Integer> {
 
     private LinkedList<Integer> ll;
+    public Stack<Integer> max_stack;
+
 
     public MyStack() {
         ll = new LinkedList<>();
+        max_stack = new Stack<>();
     }
+
 
     @Override
     public void push(Integer e) {
+        if (ll.size() == 0) {
+            max_stack.push(e);
+        }
+        if (e > max_stack.peek()) {
+            max_stack.push(e); }
         ll.addFirst(e);
+
     }
 
     @Override
     public Integer pop() {
-        Integer pop = ll.removeFirst();
-        return pop;
+        int popped_value = ll.removeFirst();
+        if (popped_value == max_stack.peek()) {
+            max_stack.pop();
+        }
+        return popped_value;
     }
 
     @Override
@@ -36,7 +50,7 @@ public class MyStack implements StackADT<Integer> {
     }
 
     public Integer maxElement() {
-        // TODO
-        return 0;
+        // Returns maximum element of the stack.
+        return max_stack.peek();
     }
 }
