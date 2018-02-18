ad1caa79c7461f732dcb5bc6735f8b9334fa9148
Anusha Datar <anushadatar@gmail.com>
2018-02-17 21:46:04 -0500

    sorting 01

--- sorting/day01/src/Problems.java
+++ sorting/day01/src/Problems.java
@@ -1,3 +1,4 @@
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -41,8 +42,55 @@ public class Problems {
      * @return the median of the stream, after each element has been added
      */
     public static double[] runningMedian(int[] inputStream) {
+
         double[] runningMedian = new double[inputStream.length];
-        // TODO
+        if (inputStream.length == 0) {
+            return runningMedian;
+        }
+        // Make two priority queues.
+        PriorityQueue<Integer> min_vals = minPQ();
+        PriorityQueue<Integer> max_vals = maxPQ();
+
+        for (int i=0; i < inputStream.length; i++) {
+            // Odd and even cases are sad, but I'm not sure how else to deal...
+            if (i % 2 == 0) {
+                if (max_vals.isEmpty() || inputStream[i] < min_vals.peek()) {
+                    max_vals.offer(inputStream[i]);
+                }
+                else {
+                    min_vals.offer(inputStream[i]);
+                }
+                if (max_vals.size() >= min_vals.size()) {
+                    runningMedian[i] = max_vals.peek();
+                }
+                else {
+                    runningMedian[i] = min_vals.peek();
+                }
+            }
+            else {
+                if (max_vals.size() > min_vals.size()) {
+                    if (inputStream[i] < max_vals.peek()) {
+                        min_vals.offer(max_vals.poll());
+                        max_vals.offer(inputStream[i]);
+                    }
+                    else {
+                        min_vals.offer(inputStream[i]);
+                    }
+                }
+                else {
+                    if (inputStream[i] > min_vals.peek()) {
+                        max_vals.offer(min_vals.poll());
+                        min_vals.offer(inputStream[i]);
+                    }
+                    else {
+                        max_vals.offer(inputStream[i]);
+                    }
+                }
+            runningMedian[i] = (((double) max_vals.peek()) + min_vals.peek())/2 ;
+            }
+
+        }
+
         return runningMedian;
     }
 
--- sorting/day01/src/QuickSort.java
+++ sorting/day01/src/QuickSort.java
@@ -21,6 +21,8 @@ public class QuickSort extends SortAlgorithm {
     @Override
     public int[] sort(int[] array) {
         // TODO: Sort the array. Make sure you avoid the O(N^2) runtime worst-case
+        shuffleArray(array);
+        quickSort(array, 0, array.length - 1);
         return array;
     }
 
@@ -35,7 +37,8 @@ public class QuickSort extends SortAlgorithm {
     public void quickSort(int[] a, int lo, int hi) {
         if (lo < hi) {
             int p = partition(a, lo, hi);
-            // TODO
+            quickSort(a, lo, p-1);
+            quickSort(a, p+1, hi);
         }
     }
 
@@ -50,7 +53,19 @@ public class QuickSort extends SortAlgorithm {
      */
     public int partition(int[] array, int lo, int hi) {
         // TODO
-        return 0;
-    }
+        int pivot = array[lo];
+        int index = hi;
+        int swap_index = hi + 1;
+        while (index > lo) {
+            if (array[index] >= pivot) {
+                swap_index--;
+                swap(array, swap_index, index);
+            }
+            index--;
+        }
+        swap_index--;
+        swap(array, lo, swap_index);
+        return swap_index;
+        }
 
 }
