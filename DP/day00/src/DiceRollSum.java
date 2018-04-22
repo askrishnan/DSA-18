4f4f8800d4f2d96767aba940710ca2ce50a2b2d7
askrishnan <anupamakrishnan@students.olin.edu>
2018-04-22 18:26:07 -0400

    Finished DP

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,39 @@
-public class DiceRollSum {
+import java.util.HashMap;
 
-    // Runtime: TODO
-    // Space: TODO
-    public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+public class DiceRollSum {
+    /**
+     * DYNAMIC PROGRAMMING STEPS
+     * 1) Define: DP[i] = number of dice rolls needs for a value of N
+     * 2) Guess: Will 1, 2, 3, 4, 5, 6 get me to the value of N?
+     * 3) RR: DP[i] = DP[n - 1] + DP[n - 2] + DP[n - 3] + DP[n - 4] + DP[n - 5] + DP[n - 6]
+     * 4) Recursion: I recurse through each value of n, subtracting the value of the rolls and add 1 to the number of sequences when n = 0
+     * 5) Solve: DP[N] is the sum of all of the sequences that result from the recursions
+     */
+    // Runtime: O(N*1)
+    // Space: 0(N)
+    public static int diceRollSumRecurs(int n, HashMap<Integer, Integer> memo) {
+        // base case
+        if (n < 0) {
+            // returns zero if coin value makes final value negative (aka it's too big)
+            return 0;
+        }
+        if (n == 0) {
+            // adds one sequence
+            return 1;
+        }
+        if (memo.containsKey(n)) {
+            // return previously calculated # of sequences for the n-value
+            return memo.get(n);
+        }
+        int DP = diceRollSumRecurs(n - 1, memo) + diceRollSumRecurs(n - 2, memo) + diceRollSumRecurs(n - 3, memo) +
+                diceRollSumRecurs(n - 4, memo) + diceRollSumRecurs(n - 5, memo) + diceRollSumRecurs(n - 6, memo);
+        memo.put(n, DP);
+        return DP;
+    }
+
+    public static int diceRollSum(int N){
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return diceRollSumRecurs(N, memo);
     }
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,37 @@
 public class LongestIncreasingSubsequence {
-
-    // Runtime: TODO
-    // Space: TODO
+    /**
+     * DYNAMIC PROGRAMMING STEPS
+     * 1) Define: DP[i] = length of subsequence
+     * 2) Guess: Which values will create the longest increasing subsequence?
+     * 3) RR: DP[i] = DP[orev longest subsequence that works] + 1
+     * 4) Memoization: I hold on to the lengths of increasing subsequences, changing them when I find a longer one
+     * 5) Solve: DP[i] is the length of the longest subsequence
+     */
+    // Runtime: O(N^2)
+    // Space: O(N)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        int longest = -1;
+        int[] DP = new int[A.length];
+
+        // starting at the end to make sure the values after i are greater
+        for (int i = A.length - 1; i >= 0; i--) {
+            // works through values after A[i]
+            for (int j = i + 1; j < A.length; j++) {
+                // checks for increasing-ness
+                if (A[i] < A[j]) {
+                    // uses lengths of preexisting increasing subsequences (chooses the longer one)
+                    if (DP[i] < DP[j] + 1) {
+                        DP[i] = DP[j] + 1;
+                    }
+                }
+            }
+            // if subsequence is longer than current longest subsequence, change value
+            if (DP[i] > longest) {
+                longest = DP[i];
+            }
+        }
+
+        // adds one to account for first pair
+        return longest + 1;
     }
 }
