122a1a4cbe2326a5a16916966a2c21e2bff8999e
askrishnan <anupamakrishnan@students.olin.edu>
2018-04-22 20:44:26 -0400

    Finished DP

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -3,31 +3,34 @@ import java.util.HashMap;
 public class DiceRollSum {
     /**
      * STEPS
-     * 1) Define: DP[i] = number of dice rolls needs for a value of N
+     * 1) Define: DP[i] = number of dice rolls needed for a value of N
      * 2) Guess: Will 1, 2, 3, 4, 5, 6 get me to the value of N?
      * 3) RR: DP[i] = DP[n - 1] + DP[n - 2] + DP[n - 3] + DP[n - 4] + DP[n - 5] + DP[n - 6]
      * 4) Recursion: I recurse through each value of n, subtracting the value of the rolls and add 1 to the number of sequences when n = 0
      * 5) Solve: DP[N] is the sum of all of the sequences that result from the recursions
+     *
+     * RUNTIME: O(6N) turns exponential runtimes to polynomial runtimes
+     * SPACE: 0(N)
      */
-    // Runtime: O(N)
-    // Space: 0(N)
-    public static int diceRollSumRecurs(int n, HashMap<Integer, Integer> memo) {
+
+    public static int diceRollSumRecurs(int N, HashMap<Integer, Integer> memo) {
         // base case
-        if (n < 0) {
+        if (N < 0) {
             // returns zero if coin value makes final value negative (aka it's too big)
             return 0;
         }
-        if (n == 0) {
+        if (N == 0) {
             // adds one sequence
             return 1;
         }
-        if (memo.containsKey(n)) {
+        if (memo.containsKey(N)) {      // MEMOIZATION STEP
             // return previously calculated # of sequences for the n-value
-            return memo.get(n);
+            return memo.get(N);
         }
-        int DP = diceRollSumRecurs(n - 1, memo) + diceRollSumRecurs(n - 2, memo) + diceRollSumRecurs(n - 3, memo) +
-                diceRollSumRecurs(n - 4, memo) + diceRollSumRecurs(n - 5, memo) + diceRollSumRecurs(n - 6, memo);
-        memo.put(n, DP);
+        // RECURSIVE STEP
+        int DP = diceRollSumRecurs(N - 1, memo) + diceRollSumRecurs(N - 2, memo) + diceRollSumRecurs(N - 3, memo) +
+                diceRollSumRecurs(N - 4, memo) + diceRollSumRecurs(N - 5, memo) + diceRollSumRecurs(N - 6, memo);
+        memo.put(N, DP);                // MEMOIZATION STEP
         return DP;
     }
 
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -3,12 +3,15 @@ public class LongestIncreasingSubsequence {
      * STEPS
      * 1) Define: DP[i] = length of subsequence
      * 2) Guess: Which values will create the longest increasing subsequence?
-     * 3) RR: DP[i] = DP[orev longest subsequence that works] + 1
+     * 3) RR: DP[i] = DP[Prev longest subsequence that works] + 1
      * 4) Memoization: I hold on to the lengths of increasing subsequences, changing them when I find a longer one
      * 5) Solve: DP[i] is the length of the longest subsequence
+     *
+     * RUNTIME: O(N^2)
+     * SPACE: O(N)
      */
-    // Runtime: O(N^2)
-    // Space: O(N)
+
+
     public static int LIS(int[] A) {
         int longest = -1;
         int[] DP = new int[A.length];
